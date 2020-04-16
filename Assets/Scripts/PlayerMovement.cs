e37c4f28f969c3151b14590c8f56046d6154c500
AJAW98 <43704211+AJAW98@users.noreply.github.com>
2020-04-16 17:34:27 +0100

    first commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+   
+    CharacterController2D controller;
+
+    public float speed = 40.0f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController2D>();
+    }
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxis("Horizontal") * speed;
+
+        if (Input.GetButtonDown("Jump")) {
+            jump = true;
+            Debug.Log("Jump");
+        }
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+    
+}
+
